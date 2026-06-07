e384582c2d94761712dda6fa9095df852c6baa5a
Eboizegbe Osaro <eboizegbeosaro1@gmail.com>
2026-06-07 18:03:03 +0100

    test: changed how the url is being accessed to a more direct one

--- src/index.js
+++ src/index.js
@@ -21,7 +21,19 @@ webpush.setVapidDetails(
 );
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:4173', process.env.CLIENT_URL ],
+    origin: function(origin, callback) {
+        const allowed = [
+            'http://localhost:5173',
+            'http://localhost:4173',
+            'https://roomie-connect-frontend.vercel.app'
+            // process.env.CLIENT_URL
+        ];
+        if (!origin || allowed.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
